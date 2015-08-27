1a5e80aa1e2edfdd2446210b2e161393913ecde6
Preston Lamb <preston.j.lamb@gmail.com>
2015-08-26 23:08:35 -0600

    working on the tutorial

--- author-quiz/public/react-prod/script.js
+++ author-quiz/public/react-prod/script.js
@@ -3,19 +3,72 @@
 	'use strict';
 
 	var Quiz = React.createClass({displayName: "Quiz",
+		getInitialState: function() {
+			return {
+				author: this.props.data[0],
+				books: this.props.data[0].books
+			};
+		},
 		propTypes: {
-			books: React.PropTypes.array.isRequired
+			data: React.PropTypes.array.isRequired
 		},
 		render: function() {
 			return (
 				React.createElement("div", null, 
-					this.props.books.map(function(book) {
-						return React.createElement(Book, {title: book});
-					})
+					React.createElement("div", {className: "row"}, 
+						React.createElement("div", {className: "col-md-4"}, 
+							React.createElement("img", {src: this.state.author.imageUrl, className: "authorimage col-md-3"})
+						), 
+						React.createElement("div", {className: "col-md-7"}, 
+							this.state.books.map(function(book) {
+								return React.createElement(Book, {title: book})
+							}, this)
+						), 
+						React.createElement("div", {className: "col-md-1"})
+					)
 				)
 			);
 		}
 	});
+
+	var data = [
+		{
+			name: 'Mark Twain',
+			imageUrl: 'public/images/authors/marktwain.jpg',
+			books: ['The Adventures of Huckleberry Finn']
+		},
+		{
+			name: 'Joseph Conrad',
+			imageUrl: 'public/images/authors/josephconrad.png',
+			books: ['Heart of Darkness']
+		},
+		{
+			name: 'J.K. Rowling',
+			imageUrl: 'public/images/authors/jkrowling.jpg',
+			imageSource: 'Wikimedia Commons',
+			imageAttribution: 'Daniel Ogren',
+			books: ['Harry Potter and the Sorcerers Stone']
+		},
+		{
+			name: 'Stephen King',
+			imageUrl: 'public/images/authors/stephenking.jpg',
+			imageSource: 'Wikimedia Commons',
+			imageAttribution: 'Pinguino',
+			books: ['The Shining','IT']
+		},
+		{
+			name: 'Charles Dickens',
+			imageUrl: 'public/images/authors/charlesdickens.jpg',
+			imageSource: 'Wikimedia Commons',
+			books: ['David Copperfield', 'A Tale of Two Cities']
+		},
+		{
+			name: 'William Shakespeare',
+			imageUrl: 'public/images/authors/williamshakespeare.jpg',
+			imageSource: 'Wikimedia Commons',
+			books: ['Hamlet', 'Macbeth', 'Romeo and Juliet']
+		}
+	];
 
 	var Book = React.createClass({displayName: "Book",
 		propTypes: {
@@ -30,6 +83,6 @@
 		}
 	});
 
-	React.render(React.createElement(Quiz, {books: ['The Lord of the Rings', 'The Iliad']}), document.getElementById("container") );
+	React.render(React.createElement(Quiz, {data: data}), document.getElementById("app") );
 
 })();
